031d6b970b450cf764bbb5d8f44ebe7c85e4d108
Kyle Ross <kyle@ktross.com>
2022-05-08 02:22:01 -0400

    Update trailer.tsx

--- components/trailer.tsx
+++ components/trailer.tsx
@@ -7,7 +7,7 @@ type Props = {
 const Trailer = ({ className }: Props) => (
     <div className={className} style={{backgroundImage: `url("/bg.jpg")` }}>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <iframe className="w-full aspect-video" id="ytplayer" src="https://www.youtube.com/embed/DtjbkBUl8cc?autoplay=1&controls=1&modestbranding=0&origin=https://www.aqtiongame.com" frame-border="0"></iframe>
+          <iframe className="w-full aspect-video" id="ytplayer" src="https://www.youtube.com/embed/DtjbkBUl8cc?rel=0autoplay=0&controls=1&modestbranding=0&origin=https://www.aqtiongame.com" frame-border="0"></iframe>
         </div>
     </div>
 )
