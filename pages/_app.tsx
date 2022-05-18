568d5c4d67ad2d69d8d4b4dddc6e3585ada533a5
Kyle Ross <kyle@ktross.com>
2022-05-17 22:13:50 -0400

    add fathom analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const fathomSiteId: string = process.env.FATHOM_SITE_ID ?? '';
+
+  useEffect(() => {
+    Fathom.load(fathomSiteId, {
+      includedDomains: ['www.aqtiongame.com'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
+
   return <Component {...pageProps} />
 }
 
