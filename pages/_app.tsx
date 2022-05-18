5f1e8d1ceafb67af26d77cc4b34d379d8334b7a4
Kyle Ross <kyle@ktross.com>
2022-05-17 22:26:48 -0400

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import Router from 'next/router';
 import * as Fathom from 'fathom-client';
 
+Router.events.on('routeChangeComplete', (as, routeProps) => {
+  if (!routeProps.shallow) {
+    Fathom.trackPageview();
+  }
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
 
   useEffect(() => {
     Fathom.load(process.env.FATHOM_SITE_ID ?? '', {
-      includedDomains: ['www.aqtiongame.com'],
+      includedDomains: ['www.aqtiongame.com']
     });
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview();
-    }
-    // Record a pageview when route changes
-    router.events.on('routeChangeComplete', onRouteChangeComplete);
-
-    // Unassign event listener
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete);
-    };
   }, []);
 
   return <Component {...pageProps} />
