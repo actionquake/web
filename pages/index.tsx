f0e5ed0f7fa5e328f87d02c34fd2c2133c9a4fa1
Dino <8dino2@gmail.com>
2025-01-08 15:31:06 -0500

    Bumped next.js version and react/react-dom

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,16 +83,16 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
   })
 
   const release: Release = {
-    id: json.id || '',
-    url: json.html_url || '',
-    version: json.tag_name || '',
-    name: json.name || '',
-    notes: json.body || '',
-    draft: json.draft || false,
-    prerelease: json.prerelease || false,
+    id: json.id,
+    url: json.html_url,
+    version: json.tag_name,
+    name: json.name,
+    notes: json.body,
+    draft: json.draft,
+    prerelease: json.prerelease,
     assets: assets,
-    created_at: json.created_at || '',
-    published_at: json.published_at || ''
+    created_at: json.created_at,
+    published_at: json.published_at
   }
 
   return {
