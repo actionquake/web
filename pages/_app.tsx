5f2a04c5d1553a152eef01ac726effd6e35bf81d
Kyle Ross <kyle@ktross.com>
2025-01-08 20:34:01 -0500

    Bump versions, remove fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react';
-import Router from 'next/router';
-import * as Fathom from 'fathom-client';
-
-Router.events.on('routeChangeComplete', (as, routeProps) => {
-  if (!routeProps.shallow) {
-    Fathom.trackPageview();
-  }
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  useEffect(() => {
-    Fathom.load('MYJRAPAB', {
-      url: 'https://restored-stay.aqtiongame.com/script.js',
-      includedDomains: ['www.aqtiongame.com']
-    });
-  }, []);
 
   return <Component {...pageProps} />
 }
